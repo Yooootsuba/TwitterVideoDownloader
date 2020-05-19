f93ef6657c31d98c39bb88c35dc58125ec16e034
Yotsuba <happy819tw@gmail.com>
2020-05-19 18:28:55 +0800

    add get_bearer_token function

--- app.php
+++ app.php
@@ -2,6 +2,7 @@
 
 
 require __DIR__ . '/vendor/autoload.php';
+use PHPHtmlParser\Dom;
 
 
 class TwitterDownloader
@@ -9,14 +10,23 @@ class TwitterDownloader
 
     function __construct($video_url)
     {
-        $this->video_url = $video_url;
+        $this->video_url    = $video_url;
+        $this->video_url_id = str_replace('https://twitter.com/i/status/', '', $video_url);
+        $this->session      = new Requests_Session();
+    }
 
+    function get_bearer_token()
+    {
+        $response = $this->session->get('https://abs.twimg.com/web-video-player/TwitterVideoPlayerIframe.cefd459559024bfb.js');
+        preg_match('/Bearer ([a-zA-Z0-9%-])+/', $response->body, $matches);
+        $bearer_token = $matches[0];
     }
 
 }
 
 
-$twitter_downloader = new TwitterDownloader('hello');
+$twitter_downloader = new TwitterDownloader('https://twitter.com/i/status/1201327902941253632');
+$twitter_downloader->get_bearer_token();
 
 
 ?>
